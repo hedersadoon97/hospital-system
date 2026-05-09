b42abfe0d7f0461182ec65d706d5f3037cd4c45b
hedersadoon97 <hedersadoon@gmail.com>
2026-05-09 18:21:17 +0300

    northflank fix

--- app.py
+++ app.py
@@ -2722,10 +2722,11 @@ import os
 if __name__ == "__main__":
 
     port = int(os.environ.get("PORT", 8080))
-
+    
     socketio.run(
-        app,
-        host="0.0.0.0",
-        port=port,
-        debug=False
-    )
+    app,
+    host="0.0.0.0",
+    port=port,
+    debug=False,
+    allow_unsafe_werkzeug=True
+)
